7930d51460a24e8706e47ae8effb9e502ae24721
Gadzbi <crg1234@gmail.com>
2022-11-04 18:52:09 +0100

    fixed bad input and refactored

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ use rodio::Sink;
 use std::fs::File;
 use std::io::BufReader;
 
+// Global variables for TRACKS
+const TRACKS: [&str; 7] = [
+    "wracajac.mp3",
+    "wracajac2.mp3",
+    "austria.mp3",
+    "kaczka.mp3",
+    "mistrze_swiata.mp3",
+    "nitek_drogi.mp3",
+    "sugerujesz_insynuacje.mp3",
+];
+const TRACKS_LEN: i32 = TRACKS.len() as i32;
+
 // Plays given audio
 fn play_audio(file_name: &str) {
     // Load output stream and sink for multithreaded audio
@@ -17,19 +29,19 @@ fn play_audio(file_name: &str) {
     sink.sleep_until_end();
 }
 
-// Displays tracks and info
-fn help(tracks: [&str; 7]) {
-    println!("-------------tracks-------------");
-    for i in 0..tracks.len() {
-        println!("{} => {}", i + 1, tracks[i]);
+// Displays TRACKS and info
+fn help() {
+    println!("-------------TRACKS-------------");
+    for i in 0..TRACKS.len() {
+        println!("{} => {}", i + 1, TRACKS[i]);
     }
     println!("0 => EXIT");
     println!("-1 => HELP");
 }
 
 // Interaction with user
-fn choose_track(tracks: [&str; 7]) {
-    help(tracks);
+fn choose_track() {
+    help();
     loop {
         // Get input from user and convert to i32
         println!("Choose a track");
@@ -37,34 +49,18 @@ fn choose_track(tracks: [&str; 7]) {
         std::io::stdin()
             .read_line(&mut input)
             .expect("Incorrect input");
-        let value: i32 = input.trim().parse().expect("Not a number");
+        let value: i32 = input.trim().parse().unwrap_or(-1);
 
         // Match the given input to a file name
         match value {
-            -1 => help(tracks),
+            -1 => help(),
             0 => return,
-            1 => play_audio(tracks[value as usize - 1]),
-            2 => play_audio(tracks[value as usize - 1]),
-            3 => play_audio(tracks[value as usize - 1]),
-            4 => play_audio(tracks[value as usize - 1]),
-            5 => play_audio(tracks[value as usize - 1]),
-            6 => play_audio(tracks[value as usize - 1]),
-            7 => play_audio(tracks[value as usize - 1]),
+            1..=TRACKS_LEN => play_audio(TRACKS[value as usize - 1]),
             _ => println!("error"),
         }
     }
 }
 fn main() -> std::io::Result<()> {
-    // Names of tracks
-    let tracks: [&str; 7] = [
-        "wracajac.mp3",
-        "wracajac2.mp3",
-        "austria.mp3",
-        "kaczka.mp3",
-        "mistrze_swiata.mp3",
-        "nitek_drogi.mp3",
-        "sugerujesz_insynuacje.mp3",
-    ];
-    choose_track(tracks);
+    choose_track();
     Ok(())
 }
